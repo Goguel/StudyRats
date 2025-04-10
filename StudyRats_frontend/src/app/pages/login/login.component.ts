97320eaee12e7f8e970e8cc30a8371c359503c84
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-10 18:29:39 -0300

    feat: Add custom messages

--- StudyRats_frontend/src/app/pages/login/login.component.ts
+++ StudyRats_frontend/src/app/pages/login/login.component.ts
@@ -4,6 +4,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginService } from '../../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-login',
@@ -24,7 +25,8 @@ export class LoginComponent {
 
   constructor(
     private router: Router,
-    private loginService: LoginService
+    private loginService: LoginService,
+    private toastService: ToastrService
   ) {
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -34,8 +36,11 @@ export class LoginComponent {
 
   submit() {
     this.loginService.login(this.loginForm.value.email, this.loginForm.value.password).subscribe({
-      next: () => console.log("sucesso"),
-      error: () => console.log("error")
+      next: () => this.toastService.success("Login feito com sucesso!"),
+      error: () => this.toastService.error(
+        "Erro ao fazer login!<br>Tente novamente mais tarde", '', 
+      { enableHtml : true }
+      ),
     })
   }
   navigate() {
