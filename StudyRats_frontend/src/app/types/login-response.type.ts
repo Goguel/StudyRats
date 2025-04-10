d8ef7f97f0a5e6ac6b682ba2baf2d84ad5c20ef5
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-10 18:13:35 -0300

    feat: Create the login-response

---
+++ StudyRats_frontend/src/app/types/login-response.type.ts
@@ -0,0 +1,4 @@
+export type LoginResponse = {  
+    token: string;   // JWT token for authentication   user: {     id: number;     name: string;     email: string;     role: string;   }; };
+    name: string;
+}
