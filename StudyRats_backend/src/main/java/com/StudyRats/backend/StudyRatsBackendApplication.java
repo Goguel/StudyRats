3c01cbcd0565c9b459768c624c13823d8da4a256
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-09 21:11:06 -0300

    init backend

---
+++ StudyRats_backend/src/main/java/com/StudyRats/backend/StudyRatsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.StudyRats.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyRatsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyRatsBackendApplication.class, args);
+	}
+
+}
---
+++ StudyRats_backend/src/test/java/com/StudyRats/backend/StudyRatsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.StudyRats.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRatsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
