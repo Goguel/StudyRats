fb38e7ffc6b1ed24ddb77d970d0c26e1f80d3548
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-10 23:36:32 -0300

    feat: Create CustomUserDetailsService

---
+++ StudyRats_backend/src/main/java/com/StudyRats/backend/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.StudyRats.backend.infra.security;
+
+
+import com.StudyRats.backend.model.User;
+import com.StudyRats.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+
+@Component
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = this.userRepository.findByEmail(username).orElseThrow(
+                () -> new UsernameNotFoundException("Usuário não encontrado")
+        );
+        return new  org.springframework.security.core.userdetails.User(
+                user.getEmail(), user.getPassword(), new ArrayList<>()
+        );
+    }
+}
