9ab1818820c6c069debd220b772a1c6590030698
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-14 17:06:05 -0300

    fix: Update Url's

--- StudyRats_frontend/src/app/services/login.service.ts
+++ StudyRats_frontend/src/app/services/login.service.ts
@@ -2,17 +2,17 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginResponse } from '../types/login-response.type';
 import { tap } from 'rxjs';
+import { environment } from "../../environments/environment";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
-  apiUrl: string ="http://localhost:8080/auth"
 
   constructor(private httpClient: HttpClient) { }
 
   login(email: string, password: string) {
-    return this.httpClient.post<LoginResponse>(this.apiUrl + "/login", { email, password }).pipe(
+    return this.httpClient.post<LoginResponse>(`${environment.apiUrl}/login`, { email, password }).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
@@ -20,7 +20,7 @@ export class LoginService {
     }
 
   signup(name: String, email: string, password: string) {
-    return this.httpClient.post<LoginResponse>(this.apiUrl + "/register", { name, email, password }).pipe(
+    return this.httpClient.post<LoginResponse>(`${environment.apiUrl}/register`, { name, email, password }).pipe(
       tap((value) => {
         sessionStorage.setItem("auth-token", value.token)
         sessionStorage.setItem("username", value.name)
