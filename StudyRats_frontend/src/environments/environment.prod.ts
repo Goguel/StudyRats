aea3b8fad97fe3979586e1eefb1f494cab6085e5
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-14 17:05:44 -0300

    feat: Create environments

---
+++ StudyRats_frontend/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://studyrats-backend.onrender.com' // coloque a URL correta aqui
+};
---
+++ StudyRats_frontend/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080' // ou a porta que o Spring Boot estiver rodando
+};
