202c04bf873a676a88f1cca6831f238537cd42af
Miguel Xavier <94547410+Goguel@users.noreply.github.com>
2025-04-21 15:40:37 -0300

    feat: Create the header

--- StudyRats_frontend/src/app/components/header/header.component.ts
+++ StudyRats_frontend/src/app/components/header/header.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import {NgOptimizedImage} from "@angular/common";
+import {BtnPrimaryLandingComponent} from "../btn-primary-landing/btn-primary-landing.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    BtnPrimaryLandingComponent
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  constructor(private router: Router) {
-  }
-  navigate() {
+   constructor(
+    private router: Router
+   ){}
+
+  login() {
     this.router.navigate(["/login"]);
+  }
+
+  singup() {
+    this.router.navigate(["/signup"]);
   }
 }
